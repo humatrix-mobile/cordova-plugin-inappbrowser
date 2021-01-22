93f2837ec83862b3312afaacdac5b6020b8530df
kaveepart <35480359+kaveepart@users.noreply.github.com>
2021-01-22 17:00:02 +0700

    fixbug import android.content.DialogInterface;

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -23,7 +23,7 @@ import android.annotation.TargetApi;
 import android.content.ComponentName;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterfac
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
